8b1dd6963f882db03dd31028e8cce97eca8176aa
arpadJM <bodiarpad@gmail.com>
2026-04-26 19:19:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // Ez segít, ha a kódodban kiterjesztés nélkül hivatkozol fájlokra
+    extensions: ['.js', '.jsx', '.json']
+  },
   build: {
     outDir: 'dist',
+    // Biztosítjuk, hogy a JSX feldolgozás ne akadjon el
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
   server: {
     port: 3000,
